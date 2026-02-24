895bf1a432c1e3a04c13c4c3a2de6797394c75b2
nhphan0505 <nhphan0505@gmail.com>
2026-02-24 17:38:57 +0700

    remove print debug

--- src/utils/show_image.py
+++ src/utils/show_image.py
@@ -1,3 +1,6 @@
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning, message="Failed to load image Python extension")
+
 import matplotlib.pyplot as plt
 import torch
 from torchvision.utils import make_grid
@@ -5,10 +8,9 @@ from src.color.lab_to_rgb import lab_to_rgb
 
 def show_img(L, ab, title = "Colorized"):
     img = torch.from_numpy(lab_to_rgb(L, ab)).permute(0,3,1,2)
-    print(img.shape)
     grid = make_grid(img, nrow=10, padding=2)
     plt.figure(figsize=(16,2))
     plt.imshow(grid.permute(1,2,0).numpy()) 
-    plt.title(title)
+    plt.title(title) 
     plt.axis('off')
-    plt.show()
+    plt.show() 
